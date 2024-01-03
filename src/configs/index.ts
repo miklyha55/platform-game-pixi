041384f4b05b9c88bb3bf43ef5268b68eefd9400
Roman Kolenko <miklyha55@yandex.ru>
2024-01-03 21:39:46 +0300

    feat(assets): load images

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,4 +1,29 @@
-export const textureAssets = {};
+import bg from "../assets/images/game/bg.png";
+
+import cat1 from "../assets/images/game/cat/cat_1.png";
+import cat2 from "../assets/images/game/cat/cat_2.png";
+
+import block1 from "../assets/images/game/blocks/block_1.png";
+import block2 from "../assets/images/game/blocks/block_2.png";
+import block3 from "../assets/images/game/blocks/block_3.png";
+
+import coin from "../assets/images/ui/coin.png";
+import life from "../assets/images/ui/life.png";
+
+export const textureAssets = {
+  bg,
+  cat: {
+    cat1,
+    cat2,
+  },
+  block: {
+    block1,
+    block2,
+    block3,
+  },
+  coin,
+  life,
+};
 
 export const fontAssets = {};
 
